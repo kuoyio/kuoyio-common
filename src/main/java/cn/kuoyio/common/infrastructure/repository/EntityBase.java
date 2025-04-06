e1ad26ed9758f2b2b7f7be37b85b4424b6dc0c2e
kuoyio <admin@kuoyio.cn>
2025-04-06 20:30:45 +0800

    version🏷️: 升级版本到0.0.17

--- src/main/java/cn/kuoyio/common/infrastructure/repository/EntityBase.java
+++ src/main/java/cn/kuoyio/common/infrastructure/repository/EntityBase.java
@@ -5,7 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.CreatedDate;
@@ -17,6 +19,8 @@ import java.time.LocalDateTime;
 @Data
 @SuperBuilder
 @MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
 @SQLRestriction("is_deleted = false")
 @EntityListeners(AuditingEntityListener.class)
 public abstract class EntityBase {
