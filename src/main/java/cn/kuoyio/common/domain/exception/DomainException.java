79b2fd590ddaf16a11372b9aa518c13eb7fb7c0b
kuoyio <admin@kuoyio.cn>
2025-04-06 22:56:19 +0800

    version🏷️: 升级版本到0.0.18

--- src/main/java/cn/kuoyio/common/domain/exception/DomainException.java
+++ src/main/java/cn/kuoyio/common/domain/exception/DomainException.java
@@ -1,7 +1,11 @@
 package cn.kuoyio.common.domain.exception;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public abstract class DomainException extends RuntimeException {
     private final HttpStatus httpStatus;
     private final String code;
